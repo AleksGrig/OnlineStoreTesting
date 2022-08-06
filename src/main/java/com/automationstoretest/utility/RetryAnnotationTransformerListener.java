4e6959e5ceeec64068381f28ac426eb8ed7c1e9d
AleksGrig <ullitap@gmail.com>
2022-08-06 11:34:32 +0300

    refactoring

--- src/main/java/com/automationstoretest/utility/RetryAnnotationTransformerListener.java
+++ src/main/java/com/automationstoretest/utility/RetryAnnotationTransformerListener.java
@@ -6,7 +6,7 @@ import java.lang.reflect.Method;
 import org.testng.IAnnotationTransformer;
 import org.testng.annotations.ITestAnnotation;
 
-public class RetryListener implements IAnnotationTransformer{
+public class RetryAnnotationTransformerListener implements IAnnotationTransformer{
   
   @SuppressWarnings("all")
   public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
