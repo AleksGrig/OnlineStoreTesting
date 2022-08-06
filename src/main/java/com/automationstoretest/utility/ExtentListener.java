73e2705e133f4a9a22fbbcc002ad97836511a4ee
AleksGrig <ullitap@gmail.com>
2022-08-06 21:21:05 +0300

    Cleaning ExtentListener

--- src/main/java/com/automationstoretest/utility/ExtentListener.java
+++ src/main/java/com/automationstoretest/utility/ExtentListener.java
@@ -18,25 +18,19 @@ public class ExtentListener extends ExtentManager implements ITestListener {
 	}
 
 	public void onTestSuccess(ITestResult result) {
-		if (result.getStatus() == ITestResult.SUCCESS) {
-			test.get().log(Status.PASS, "Pass Test case is: " + result.getName());
-		}
+		test.get().log(Status.PASS, "Pass Test case is: " + result.getName());
 	}
 
 	public void onTestFailure(ITestResult result) {
-		if (result.getStatus() == ITestResult.FAILURE) {
-			test.get().log(Status.FAIL,
-					MarkupHelper.createLabel(result.getName() + " - Test Case Failed", ExtentColor.RED));
-			test.get().log(Status.FAIL,
-					MarkupHelper.createLabel(result.getThrowable() + " - Test Case Failed", ExtentColor.RED));
-			String imgPath = Base.screenshot(Base.getDriver(), result.getName());
-			test.get().fail("ScreenShot is Attached", MediaEntityBuilder.createScreenCaptureFromPath(imgPath).build());
-		}
+		test.get().log(Status.FAIL,
+				MarkupHelper.createLabel(result.getName() + " - Test Case Failed", ExtentColor.RED));
+		test.get().log(Status.FAIL,
+				MarkupHelper.createLabel(result.getThrowable() + " - Test Case Failed", ExtentColor.RED));
+		String imgPath = Base.screenshot(Base.getDriver(), result.getName());
+		test.get().fail("ScreenShot is Attached", MediaEntityBuilder.createScreenCaptureFromPath(imgPath).build());
 	}
 
 	public void onTestSkipped(ITestResult result) {
-		if (result.getStatus() == ITestResult.SKIP) {
-			test.get().log(Status.SKIP, "Skipped Test case is: " + result.getName());
-		}
+		test.get().log(Status.SKIP, "Skipped Test case is: " + result.getName());
 	}
 }
