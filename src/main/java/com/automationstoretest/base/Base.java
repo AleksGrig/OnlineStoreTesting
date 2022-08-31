628e6e297d2e63f91e18ac368c21c2c3a34e998e
AleksGrig <ullitap@gmail.com>
2022-08-31 21:55:17 +0300

    default screenshot location

--- src/main/java/com/automationstoretest/base/Base.java
+++ src/main/java/com/automationstoretest/base/Base.java
@@ -94,7 +94,7 @@ public class Base {
         System.getProperty("launch").equals("jenkins") ? true : false;
 		logger.info(() -> "jenkins: " + jenkins);
     String destination = jenkins ? """
-        C:/Users/Alex/Workbench/Jenkins_OnlineStoreTesting/screenshots/%s_%s.png
+        http://localhost:8080/job/OnlineStoreTesting/ws/screenshots/%s_%s.png
         """.formatted(filename, date) : """
         %s/screenshots/%s_%s.png""".formatted(userDir, filename, date);
     logger.info(() -> "destination: " + destination);
